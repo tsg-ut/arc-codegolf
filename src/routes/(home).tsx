8d5a89189adf022447ca3b893d8cf10a4e952a5a
Koki Takahashi <hakatasiloving@gmail.com>
2025-08-05 08:42:36 +0900

    feat: implement agreement section with styled components for user consent

--- src/routes/(home).tsx
+++ src/routes/(home).tsx
@@ -6,36 +6,47 @@ import {useAuth} from 'solid-firebase';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, Nav, Navbar} from 'solid-bootstrap';
 
+import styles from './(home).module.css';
+
 const HomeLayout = (props: RouteSectionProps) => {
 	const authState = useAuth(auth);
 
 	return (
 		<Switch>
 			<Match when={!authState.data}>
-				<div>
-					<div>
-						<p>
-							このサイトは、TSG部員のみがアクセスできるコードゴルフのサイトです。
-						</p>
-						<p>サイトにアクセスするには以下の条項に同意する必要があります。</p>
-						<ol>
-							<li>
-								このサイトに提出したコードや収集された情報が博多市によって他の目的に使用されることに同意します。
-								<ul>
-									<li>
-										なお、提出されたコードを利用して賞金などを獲得した場合、提出者の貢献の割合に応じて任意で賞金を分配することを予定しています。
-									</li>
-								</ul>
-							</li>
-							<li>
-								このサイトの内容をTSG部員以外に公開しないことに同意します。
-							</li>
-						</ol>
+				<Container class={styles.agreementContainer}>
+					<div class={styles.agreementBox}>
+						<div class={styles.agreement}>
+							<h1>TSG ARC Codegolf</h1>
+							<p>
+								このサイトは、TSG部員のみがアクセスできるコードゴルフのサイトです。
+							</p>
+							<p>
+								サイトにアクセスするには以下の条項に同意する必要があります。
+							</p>
+							<ol>
+								<li>
+									このサイトに提出したコードや収集された情報が博多市によって他の目的に使用されることに同意します。
+									<ul>
+										<li>
+											なお、提出されたコードを利用して賞金などを獲得した場合、提出者の貢献の割合に応じて賞金を分配することを予定しています。(分配方法は今後決定します)
+										</li>
+									</ul>
+								</li>
+								<li>
+									このサイトの内容をTSG部員以外に公開しないことに同意します。
+								</li>
+							</ol>
+						</div>
+						<button
+							type="button"
+							onClick={signIn}
+							class={styles.agreementButton}
+						>
+							同意してログイン
+						</button>
 					</div>
-					<button type="button" onClick={signIn}>
-						同意してログイン
-					</button>
-				</div>
+				</Container>
 			</Match>
 			<Match when={authState.data}>
 				<div>
